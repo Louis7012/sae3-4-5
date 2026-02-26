1fd639a6e7d516288f5a8194f89f6d41b891d649
Robin-p07 <robin.peigney@edu.univ-fcomte.fr>
2026-02-26 13:53:33 +0100

    Refactor database connection to use environment variables

--- S2_SAE_2026_etu_v1/connexion_db.py
+++ S2_SAE_2026_etu_v1/connexion_db.py
@@ -2,21 +2,22 @@ from flask import Flask, request, render_template, redirect, url_for, abort, fla
 
 import pymysql.cursors
 
+import os
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('/home/sae345g14/sae2.345suj14/S2_SAE_2025_etu_v1')  # adjust as appropriate (avec le dossier où se trouve le fichier .env et app.py)
+load_dotenv(os.path.join(project_folder, '.env'))
+
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        #
-        db = g._database = pymysql.connect(
-            host="localhost",
-            user="rpeig",
-            password="secret",
-            database="s2_BDD",
+    if 'db' not in g:
+        g.db =  pymysql.connect(
+            host=os.environ.get("HOST"),
+            user=os.environ.get("LOGIN"),
+            password=os.environ.get("PASSWORD"),
+            database=os.environ.get("DATABASE"),
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
-        # à activer sur les machines personnelles :
-        activate_db_options(db)
-    return db
+    return g.db
 
 def activate_db_options(db):
     cursor = db.cursor()
@@ -37,7 +38,7 @@ def activate_db_options(db):
     if result:
         if result['Value'] != '0':
             print('MYSQL : valeur de la variable globale lower_case_table_names differente de 0')   # mettre en commentaire
-            #cursor.execute("SET GLOBAL lower_case_table_names = 0")
+            cursor.execute("SET GLOBAL lower_case_table_names = 0")
             db.commit()
         else :
             print('MYSQL : variable globale lower_case_table_names=0  ok')    # mettre en commentaire
